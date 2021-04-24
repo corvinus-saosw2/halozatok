3e5ee3d611445c4d0461caec90a59f7dcf1741c1
Ábel <bel@DESKTOP-CUL402R>
2021-04-24 12:13:28 +0200

    9gyak

--- HajosTeszt/HajosTeszt/wwwroot/Hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/Hetedik.js
@@ -3,9 +3,10 @@ var kérdések;
 var kérdésSorszám = 0;
 
 function letöltés() {
-    fetch('questions.json')
+    fetch('/questions/4')
         .then(response => response.json())
-        .then(data => letöltésBefejeződött(data));
+        .then(data => console.log(data)
+        );
 }
 
 function letöltésBefejeződött(k) {
@@ -15,28 +16,27 @@ function letöltésBefejeződött(k) {
     kérdésMegjelenítés(0);
 }
 
-var kérdésMegjelenítés = function (kérdésSzáma) {
-
-    let kérdés_szöveg = document.getElementById("kérdés_szöveg");
-    let kép = document.getElementById("kép1");
-    let válasz1 = document.getElementById("válasz1");
-    let válasz2 = document.getElementById("válasz2");
-    let válasz3 = document.getElementById("válasz3");
-
-    kérdés_szöveg.innerHTML = kérdések[kérdésSzáma].questionText
-
-    if (kérdések[kérdésSzáma].image != "") {
-        kép.src = "https://szoft1.comeback.hu/hajo/" + kérdések[kérdésSzáma].image;
-    }
-    else {
-        kép.src = "";
-    }
-
-    válasz1.innerText = kérdések[kérdésSzáma].answer1
-    válasz2.innerText = kérdések[kérdésSzáma].answer2
-    válasz3.innerText = kérdések[kérdésSzáma].answer3
+function kérdésMegjelenítés(kérdés) {
+    console.log(kérdés);
+    document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
+    document.getElementById("válasz1").innerText = kérdés.answer1
+    document.getElementById("válasz2").innerText = kérdés.answer2
+    document.getElementById("válasz3").innerText = kérdés.answer3
+    document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
 }
 
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(data => kérdésMegjelenítés(data));
+} 
 
 window.onload = () => {
 
